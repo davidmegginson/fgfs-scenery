b0eecb7c3ec30dd42a6e28cffb6d5a84b8b755a2
David Megginson <david.megginson@megginson.com>
2016-12-27 13:13:08 -0500

    Expanded range.

--- make-scenery.sh
+++ make-scenery.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-MIN_LAT=44
-MAX_LAT=46
+MIN_LAT=43
+MAX_LAT=47
 MIN_LON=-77
-MAX_LON=-75
+MAX_LON=-73
 
 INPUTS="AirportArea SRTM-3 AirportObj landmass highway railway wetland water-area water-line quarry greenspace builtup-area nature builtup-point vmap0-veg vmap0-builtup"
 
@@ -12,4 +12,4 @@ echo Inputs: $INPUTS
 rm -rf output/Terrain
 rm -rf work/Shared
 tg-construct --threads --priorities=./priorities.txt --work-dir=./work --output-dir=./output/Terrain --min-lon=$MIN_LON --max-lon=$MAX_LON --min-lat=$MIN_LAT --max-lat=$MAX_LAT $INPUTS
-#tg-construct --threads --priorities=./priorities.txt --work-dir=./work --output-dir=./output/Terrain --tile-id=1712601 $INPUTS
+#tg-construct --threads --priorities=./priorities.txt --work-dir=./work --output-dir=./output/Terrain --tile-id=1729002 $INPUTS
