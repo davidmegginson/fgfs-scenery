bf294edda7f3df852c66cec7477142fde1169e9b
David Megginson <david.megginson@megginson.com>
2016-12-27 13:13:54 -0500

    Changed to merge into full airport list.

--- merge-airports.sh
+++ merge-airports.sh
@@ -2,6 +2,8 @@
 
 rm -f data/airports/apt.dat
 
+cat data/airports/default-apt.dat > data/airports/apt.dat
+
 for file in airports/*.apt.dat; do
     echo Merging $file
     cat $file | grep -v '^1000' | grep -v '^99' >> data/airports/apt.dat
